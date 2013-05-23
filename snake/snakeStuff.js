cf6e1862d7b73760cdca2980a08a5b028c2332ad
Cody and Connor <Cody+Connor@appacademy.io>
2013-05-23 10:44:49 -0400

    Snake finished

--- snake/snakeStuff.js
+++ snake/snakeStuff.js
@@ -2,6 +2,7 @@ function Board(size) {
   this.size = size;
 
   this.grid = [];
+  this.pellet = [];
 
   for(var i = 0; i < size; i++) {
     var new_arr = []
@@ -23,9 +24,11 @@ Board.prototype.makePellet = function(){
     var y = Math.floor(Math.random()*this.size);
     if (!this.grid[x][y]) {
       this.setSpot([x,y], "x"); //value of pellet
+      this.pellet = [x,y];
       this.pelletFlag = true;
     }
   }
+  this.pelletFlag = false;
 };
 
 Board.prototype.clearSpot = function(spot) {
@@ -42,7 +45,10 @@ Board.prototype.inBoard = function(spot) {
 };
 
 Board.prototype.getSpot = function(spot) {
-  return this.grid[spot[0]][spot[1]];
+  if( this.inBoard(spot)){
+    return this.grid[spot[0]][spot[1]];
+  }
+  return false;
 }
 
 // left up right down.
@@ -68,20 +74,30 @@ Snake.prototype.advance = function() {
   var newSquare = [ this.corpus[0][0] + this.direction[0],
                      this.corpus[0][1] + this.direction[1]];
 
-  this.board.clearSpot(this.corpus.pop());
+  var spotValue = this.board.getSpot(newSquare);
 
-  if (this.board.inBoard(newSquare) && !this.board.getSpot(newSquare)){
+  if( ! (spotValue ==="x")){
+    this.board.clearSpot(this.corpus.pop());
+  } else {
+    this.board.pellet = 0;
+    this.board.makePellet();
+  }
+
+  console.log(this.board.inBoard(newSquare));
+  if (this.board.inBoard(newSquare) && (!spotValue || spotValue === "x")){
     this.board.setSpot(newSquare, "S");
     this.corpus.unshift(newSquare);
   } else {
     this.alive = false;
   }
+
 };
 
 Snake.prototype.turn = function(delta){
   var inverse = this.direction;
   inverse = [inverse[0] * (-1), inverse[1] * (-1)];
-
+  console.log("Inverse: " + inverse);
+  console.log("Delta: " + delta);
   if (!(inverse[0] === delta[0] && inverse[1] === delta[1])) {
     this.direction = delta;
   }
@@ -96,6 +112,10 @@ var render = function(snake){
 
   var $cells = $(".cell")
   $cells.removeClass("s");
+  $cells.removeClass("food");
+
+  index = snake.board.pellet[0]*10 + snake.board.pellet[1];
+  $cells.eq(index).addClass("food");
 
   for(var i = 0; i < snake.corpus.length; i++) {
     index = snake.corpus[i][0]*10 + snake.corpus[i][1];
@@ -108,8 +128,9 @@ var render = function(snake){
 
 $(document).ready(function() {
 
-  var board = new Board(10);
-  var snake = new Snake(board);
+  board = new Board(10);
+  board.makePellet();
+  snake = new Snake(board);
   snake.startPos();
 
   var converter = {
@@ -119,15 +140,38 @@ $(document).ready(function() {
     40: [1,0]
   };
 
-  window.setInterval(
-    function () {
+  var timerId;
+
+  var snakeLoop = function (snake) {
+
+    if (snake.alive){
       snake.advance();
       render(snake);
-    }, 1000);
+    }
+    else {
+      window.clearInterval(timerId);
+      console.log(timerId);
+      alert("You died.  How very, very sad.... for you.");
+    }
+
+  };
+
+  timerId = window.setInterval(function(){snakeLoop(snake)}, 500);
 
   $('html').keydown(function (event) {
     console.log("You pressed keycode: " + event.keyCode);
-    snake.turn(converter[event.keyCode]);
+
+    snake.turn(converter[event.keyCode] || snake.direction);
+
+    if(!snake.alive){
+      console.log("IM DEAD!")
+      board = new Board(10);
+      board.makePellet();
+      snake = new Snake(board);
+      snake.direction = DELTAS[1];
+      snake.startPos();
+      timerId = window.setInterval(function(){snakeLoop(snake)}, 500);
+    }
   });
 
 
